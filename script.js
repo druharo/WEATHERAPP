2874c6d0fc87f7cb11ccab3e434496111e65502d
druharo <111248621+druharo@users.noreply.github.com>
2022-10-10 20:20:34 +0300

    added style.css file and getting the 5 day forecast working

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var weatherApiKey = "80ca409d1e8c4b329ee163000220910";
 
 var weatherApi = "http://api.weatherapi.com/v1/forecast.json";
@@ -30,6 +29,95 @@ function updateDivs(data) {
     var humidity = data.current.humidity;
     $('#spanHumidity').html(humidity);
 
+    // update forecast for day zero
+    var forecast = data.forecast.forecastday[0];
+    // date
+    var fdate = forecast.date;
+    $('#divDate0').html(fdate);
+    // icon
+    var condition = forecast.day.condition.icon;
+    $('#spanIcon0').attr('src', condition);
+    // temp
+    var temp = forecast.day.avgtemp_c;
+    $('#spanTemp0').html(temp);
+    // wind maxwind_mph
+    var wind = forecast.day.maxwind_mph;
+    $('#spanWind0').html(wind);
+    // update Humidity
+    var humidity = forecast.day.avghumidity;
+    $('#spanHumidity0').html(humidity);
+
+    // update forecast for day one
+    var forecast = data.forecast.forecastday[1];
+    // date
+    var fdate = forecast.date;
+    $('#divDate1').html(fdate);
+    // icon
+    var condition = forecast.day.condition.icon;
+    $('#spanIcon1').attr('src', condition);
+    // temp
+    var temp = forecast.day.avgtemp_c;
+    $('#spanTemp1').html(temp);
+    // wind maxwind_mph
+    var wind = forecast.day.maxwind_mph;
+    $('#spanWind1').html(wind);
+    // update Humidity
+    var humidity = forecast.day.avghumidity;
+    $('#spanHumidity1').html(humidity);
+
+    // update forecast for day two
+    var forecast = data.forecast.forecastday[2];
+    // date
+    var fdate = forecast.date;
+    $('#divDate2').html(fdate);
+    // icon
+    var condition = forecast.day.condition.icon;
+    $('#spanIcon2').attr('src', condition);
+    // temp
+    var temp = forecast.day.avgtemp_c;
+    $('#spanTemp2').html(temp);
+    // wind maxwind_mph
+    var wind = forecast.day.maxwind_mph;
+    $('#spanWind2').html(wind);
+    // update Humidity
+    var humidity = forecast.day.avghumidity;
+    $('#spanHumidity2').html(humidity);
+
+    // update forecast for day three
+    var forecast = data.forecast.forecastday[3];
+    // date
+    var fdate = forecast.date;
+    $('#divDate3').html(fdate);
+    // icon
+    var condition = forecast.day.condition.icon;
+    $('#spanIcon3').attr('src', condition);
+    // temp
+    var temp = forecast.day.avgtemp_c;
+    $('#spanTemp3').html(temp);
+    // wind maxwind_mph
+    var wind = forecast.day.maxwind_mph;
+    $('#spanWind3').html(wind);
+    // update Humidity
+    var humidity = forecast.day.avghumidity;
+    $('#spanHumidity3').html(humidity);
+
+    // update forecast for day four
+    var forecast = data.forecast.forecastday[4];
+    // date
+    var fdate = forecast.date;
+    $('#divDate4').html(fdate);
+    // icon
+    var condition = forecast.day.condition.icon;
+    $('#spanIcon4').attr('src', condition);
+    // temp
+    var temp = forecast.day.avgtemp_c;
+    $('#spanTemp4').html(temp);
+    // wind maxwind_mph
+    var wind = forecast.day.maxwind_mph;
+    $('#spanWind4').html(wind);
+    // update Humidity
+    var humidity = forecast.day.avghumidity;
+    $('#spanHumidity4').html(humidity);
 }
 
 function handleSearchClick(event) {
